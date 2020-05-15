8405d01f49daa15ded5a5c4e14336b6ee8ca591e
Ayupancasona <60374898+Ayupancasona@users.noreply.github.com>
2020-05-16 00:38:50 +0700

    Add files via upload

---
+++ Tugas/Floating_Action_Button/app/src/main/java/com/example/floatingactionbutton/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.floatingactionbutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab1.setOnClickListener {
+            Toast.makeText(this,"Done", Toast.LENGTH_LONG).show()
+        }
+    }
+    }
+
